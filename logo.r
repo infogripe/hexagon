46a739165a07d86025162e51eee0e96f04771a12
Leo Bastos <lsbastos.fiocruz@gmail.com>
2025-04-08 15:45:30 -0300

    Create logo.r

---
+++ logo.r
@@ -0,0 +1,8 @@
+library(hexSticker)
+
+sticker("infogripe.png", package="", 
+        h_color = "#233264", h_fill = "white", 
+        # p_size=20, 
+        s_x=1, s_y=1, 
+        s_width=.85,
+        filename="infogripe_hex.svg")
